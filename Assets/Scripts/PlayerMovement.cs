fa0ed68fb60cbf4bce35272be2c5f542d7e21d45
Danylo Sapizhuk <danylosapizhuk@Danylos-MacBook-Pro.local>
2025-08-05 18:11:45 +0200

    Spikes commit

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -99,7 +99,7 @@ public class PlayerMovement : MonoBehaviour
 
     void Die()
     {
-        if (myBodyCollider2D.IsTouchingLayers(LayerMask.GetMask("Enemies")))
+        if (myBodyCollider2D.IsTouchingLayers(LayerMask.GetMask("Enemies", "Hazards")))
         {
             isAlive = false;
             myAnimator.SetTrigger("Dying");
