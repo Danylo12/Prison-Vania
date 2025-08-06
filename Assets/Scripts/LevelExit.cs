cf3cb4e7b1442aa92bb08e891c10acc5759e891b
Danylo Sapizhuk <danylosapizhuk@Danylos-MacBook-Pro.local>
2025-08-06 14:02:08 +0200

    Player Lives commit

--- Assets/Scripts/LevelExit.cs
+++ Assets/Scripts/LevelExit.cs
@@ -9,7 +9,11 @@ public class LevelExit : MonoBehaviour
     [SerializeField] float levelLoadDelay = 1f;
     private void OnTriggerEnter2D(Collider2D other)
     {
-        StartCoroutine(LoadNextLevel());
+        if (other.tag == "Player")
+        {
+            StartCoroutine(LoadNextLevel());
+        }
+        
         
     }
 
